1effa65b4a82480f6028997802acec226eb7d267
Alex <statueofibberty@gmail.com>
2019-10-13 19:02:51 -0400

    Initial Commit

---
+++ TipUpTooltip/TipUpTooltip.h
@@ -0,0 +1,19 @@
+//
+//  TipUpTooltip.h
+//  TipUpTooltip
+//
+//  Created by Alex on 10/13/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TipUpTooltip.
+FOUNDATION_EXPORT double TipUpTooltipVersionNumber;
+
+//! Project version string for TipUpTooltip.
+FOUNDATION_EXPORT const unsigned char TipUpTooltipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TipUpTooltip/PublicHeader.h>
+
+
